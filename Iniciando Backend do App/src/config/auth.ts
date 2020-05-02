a68496f901d74e0a371b0f366f39bcfc662558f8
Daniel Sousa <daniel_sousa.unb@outlook.com>
2020-05-01 23:36:59 -0300

    :sparkles: Finishing back-end

---
+++ Iniciando Backend do App/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '47aeb8267eff2a67b55d691f2a266ee0',
+    expiresIn: '1d',
+  },
+};
---
+++ Iniciando Backend do App/src/routes/sessions.routes.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+
+import AuthenticateUserService from '../services/AuthenticateUserSevice';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (request, response) => {
+  const { email, password } = request.body;
+
+  const authenticateUser = new AuthenticateUserService();
+
+  // Clareza na hora de enviar informações
+  const { user, token } = await authenticateUser.execute({
+    email,
+    password,
+  });
+
+  delete user.password;
+
+  return response.json({ user, token });
+});
+
+export default sessionsRouter;
