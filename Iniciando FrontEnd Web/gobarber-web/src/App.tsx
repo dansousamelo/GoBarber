d1410e3fb7b4987d865614387d1764ee8f6897fc
Daniel Sousa <daniel_sousa.unb@outlook.com>
2020-05-14 10:18:40 -0300

    :sparkles: Creating Login Page

---
+++ Iniciando FrontEnd Web/gobarber-web/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SignIn from './pages/Signin';
+import GlobalStyle from './styles/global';
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
+
+export default App;
