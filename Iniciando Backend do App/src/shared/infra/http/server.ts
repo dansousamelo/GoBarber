45deb40bfda98567e77bcc52f40e070754493256
Daniel de Sousa Oliveira Melo Veras <daniel_sousa.unb@outlook.com>
2020-08-12 11:02:50 -0300

    :sparkles: Validating params of routes

--- Iniciando Backend do App/src/shared/infra/http/server.ts
+++ Iniciando Backend do App/src/shared/infra/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate';
 
 import 'express-async-errors';
 
@@ -21,6 +22,8 @@ app.use(express.json());
 /* Criaremos uma rota para saber os arquivos que fizemos upload */
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
+
+app.use(errors());
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
