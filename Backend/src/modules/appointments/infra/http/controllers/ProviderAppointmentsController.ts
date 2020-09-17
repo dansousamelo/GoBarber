83318bf558a7487ac28c2ba0fcf7e27534dfd1a1
Daniel de Sousa Oliveira Melo Veras <daniel_sousa.unb@outlook.com>
2020-09-17 14:29:59 -0300

    :alien: Adding eager loading

--- Backend/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ Backend/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -6,7 +6,7 @@ import ListProvidersAppointmentsService from '@modules/appointments/services/Lis
 export default class ProviderAppointmentsController {
   public async index(request: Request, response: Response): Promise<Response> {
     const provider_id = request.user.id;
-    const { day, month, year } = request.body;
+    const { day, month, year } = request.query;
 
     const listProvidersAppointments = container.resolve(
       ListProvidersAppointmentsService
@@ -14,9 +14,9 @@ export default class ProviderAppointmentsController {
 
     const appointments = await listProvidersAppointments.execute({
       provider_id,
-      day,
-      month,
-      year,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
     });
 
     return response.json(appointments);
